ae5d6ef0ae126047e9b7d3cfac1ca05b47df33ba
Lasreex <lucas.randop4c4@gmail.com>
2026-09-20 10:35:16 +0200

    ajout des ports autorisées dans security group

--- main.tf
+++ main.tf
@@ -85,6 +85,20 @@ resource "aws_security_group" "sg_haproxy" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 1936
+    to_port     = 1936
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
   egress {
     from_port   = 0
